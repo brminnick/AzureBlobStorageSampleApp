a9e31f0d6646063cf9b7c3ed97ecc603a7fdebe0
brminnick <brandon.r.minnick@gmail.com>
2017-08-30 15:57:56 -0700

    Added Blob Storage Static Class

---
+++ AzureBlobStorageSampleApp.Functions/AzureBlobStorageSampleApp.BlobStorage/BlobStorageService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+
+namespace AzureBlobStorageSampleApp.BlobStorage
+{
+    public static class BlobStorageService
+    {
+        #region Constant Fields
+        readonly static Lazy<string> _connectionStringHolder = new Lazy<string>(() => ConfigurationManager.ConnectionStrings["BlobStorageConnectionString"].ConnectionString);
+        readonly static Lazy<CloudStorageAccount> _storageAccountHolder = new Lazy<CloudStorageAccount>(() => CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting(ConnectionString)));
+        #endregion
+
+        #region Properties
+        static string ConnectionString => _connectionStringHolder.Value;
+        static CloudStorageAccount StorageAccount => _storageAccountHolder.Value;
+        #endregion
+
+        #region Methods
+        public static void SavePhotoToBlobStorage()
+        {
+            
+        }
+#endregion
+    }
+}
--- AzureBlobStorageSampleApp.Functions/GetBlobFunction.cs
+++ AzureBlobStorageSampleApp.Functions/GetBlobFunction.cs
@@ -1,10 +1,11 @@
-using System.Linq;
 using System.Net;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 
 namespace AzureBlobStorageSampleApp.Functions
 {
