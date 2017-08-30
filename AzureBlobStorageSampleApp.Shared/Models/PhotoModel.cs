746cec3a2819eae273ec22b1b093e2f43fe1e32f
Brandon Minnick <brminnick@users.noreply.github.com>
2017-08-30 12:44:58 -0700

    Added PutBlob and GetBlob Functions

---
+++ AzureBlobStorageSampleApp.Shared/Models/PhotoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AzureBlobStorageSampleApp
+{
+    public class PhotoModel : IBaseModel
+    {
+        public PhotoModel() => Id = Guid.NewGuid().ToString();
+
+        public string Id { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+    }
+}
