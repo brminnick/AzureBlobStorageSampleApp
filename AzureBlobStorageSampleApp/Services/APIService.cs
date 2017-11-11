f7b8f67d346854b2a833ed0313665e83558cdffe
brminnick <brandon.r.minnick@gmail.com>
2017-11-11 08:50:08 -0500

    Added BaseHttpClientService

--- AzureBlobStorageSampleApp/Services/APIService.cs
+++ AzureBlobStorageSampleApp/Services/APIService.cs
@@ -1,187 +1,20 @@
-﻿using System;
-using System.IO;
-using System.Net;
-using System.Text;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
-using System.Net.Http.Headers;
 using System.Collections.Generic;
-
-using Newtonsoft.Json;
-
-using Xamarin.Forms;
 
 using AzureBlobStorageSampleApp.Shared;
 using AzureBlobStorageSampleApp.Mobile.Shared;
 
 namespace AzureBlobStorageSampleApp
 {
-    public static class APIService
+    abstract class APIService : BaseHttpClientService
     {
-        #region Constant Fields
-        static readonly JsonSerializer _serializer = new JsonSerializer();
-        static readonly HttpClient _client = CreateHttpClient(TimeSpan.FromSeconds(60));
-        #endregion
-
-        #region Fields
-        static int _networkIndicatorCount = 0;
-        #endregion
-
         #region Methods
-        public static async Task<List<PhotoModel>> GetAllPhotoModels() =>
-            await GetDataObjectFromAPI<List<PhotoModel>>($"{BackendConstants.GetAllPhotosUrl}");
+        public static Task<List<PhotoModel>> GetAllPhotoModels() =>
+            GetDataObjectFromAPI<List<PhotoModel>>($"{BackendConstants.GetAllPhotosUrl}");
 
-        public static async Task<HttpResponseMessage> PostPhotoBlob(PhotoBlobModel photoBlob, string photoTitle) =>
-            await PostObjectToAPI($"{BackendConstants.PostPhotoBlobUrl}{photoTitle}{BackendConstants.PostPhotoBlobFunctionKey}", photoBlob);
-
-        static async Task<T> GetDataObjectFromAPI<T>(string apiUrl) =>
-            await GetDataObjectFromAPI<T, object>(apiUrl);
-
-        static async Task<T> GetDataObjectFromAPI<T, U>(string apiUrl, U data = default(U))
-        {
-            var stringPayload = string.Empty;
-
-            if (data != null)
-                stringPayload = await Task.Run(() => JsonConvert.SerializeObject(data)).ConfigureAwait(false);
-
-            var httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
-
-            try
-            {
-                UpdateActivityIndicatorStatus(true);
-
-                using (var stream = await _client.GetStreamAsync(apiUrl).ConfigureAwait(false))
-                using (var reader = new StreamReader(stream))
-                using (var json = new JsonTextReader(reader))
-                {
-                    if (json == null)
-                        return default(T);
-
-                    return await Task.Run(() => _serializer.Deserialize<T>(json)).ConfigureAwait(false);
-                }
-            }
-            catch (Exception e)
-            {
-                DebugServices.Log(e);
-                return default(T);
-            }
-            finally
-            {
-                UpdateActivityIndicatorStatus(false);
-            }
-        }
-
-        static async Task<HttpResponseMessage> PostObjectToAPI<T>(string apiUrl, T data)
-        {
-            var stringPayload = await Task.Run(() => JsonConvert.SerializeObject(data)).ConfigureAwait(false);
-
-            var httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
-            try
-            {
-                UpdateActivityIndicatorStatus(true);
-
-                return await _client.PostAsync(apiUrl, httpContent).ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                DebugServices.Log(e);
-                return null;
-            }
-            finally
-            {
-                UpdateActivityIndicatorStatus(false);
-            }
-        }
-
-        static async Task<HttpResponseMessage> PatchObjectToAPI<T>(string apiUrl, T data)
-        {
-            var stringPayload = await Task.Run(() => JsonConvert.SerializeObject(data)).ConfigureAwait(false);
-
-            var httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
-
-            var httpRequest = new HttpRequestMessage
-            {
-                Method = new HttpMethod("PATCH"),
-                RequestUri = new Uri(apiUrl),
-                Content = httpContent
-            };
-
-            try
-            {
-                UpdateActivityIndicatorStatus(true);
-
-                return await _client.SendAsync(httpRequest).ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                DebugServices.Log(e);
-                return null;
-            }
-            finally
-            {
-                UpdateActivityIndicatorStatus(false);
-            }
-        }
-
-        static Task<HttpResponseMessage> DeleteObjectFromAPI(string apiUrl)
-        {
-            var httpRequest = new HttpRequestMessage(HttpMethod.Delete, new Uri(apiUrl));
-
-            try
-            {
-                UpdateActivityIndicatorStatus(true);
-
-                return _client.SendAsync(httpRequest);
-            }
-            catch (Exception e)
-            {
-                DebugServices.Log(e);
-                return null;
-            }
-            finally
-            {
-                UpdateActivityIndicatorStatus(false);
-            }
-        }
-
-        static void UpdateActivityIndicatorStatus(bool isActivityIndicatorDisplayed)
-        {
-            var baseNavigationPage = Application.Current.MainPage as NavigationPage;
-            var currentPage = baseNavigationPage.CurrentPage as ContentPage;
-            var currentViewModel = currentPage.BindingContext as BaseViewModel;
-
-            if (isActivityIndicatorDisplayed)
-            {
-                currentViewModel.IsInternetConnectionActive = true;
-                _networkIndicatorCount++;
-            }
-            else if (--_networkIndicatorCount <= 0)
-            {
-                currentViewModel.IsInternetConnectionActive = false;
-                _networkIndicatorCount = 0;
-            }
-        }
-
-        static HttpClient CreateHttpClient(TimeSpan timeout)
-        {
-            HttpClient client;
-
-            switch (Device.RuntimePlatform)
-            {
-                case Device.iOS:
-                case Device.Android:
-                    client = new HttpClient();
-                    break;
-                default:
-                    client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip });
-                    break;
-
-            }
-            client.Timeout = timeout;
-            client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
-
-            return client;
-        }
+        public Task<HttpResponseMessage> PostPhotoBlob(PhotoBlobModel photoBlob, string photoTitle) =>
+            PostObjectToAPI($"{BackendConstants.PostPhotoBlobUrl}{photoTitle}{BackendConstants.PostPhotoBlobFunctionKey}", photoBlob);
         #endregion
     }
 }
