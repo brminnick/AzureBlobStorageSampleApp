40e320f694475aa99ec5a0d57ba2fbaf3824de7c
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-02-27 14:46:15 -0800

    Updated NuGet Packages

--- AzureBlobStorageSampleApp.Droid/MainActivity.cs
+++ AzureBlobStorageSampleApp.Droid/MainActivity.cs
@@ -10,6 +10,12 @@ namespace AzureBlobStorageSampleApp.Droid
     [Activity(Label = "AzureBlobStorageSampleApp.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -23,12 +29,6 @@ namespace AzureBlobStorageSampleApp.Droid
             EntryCustomReturn.Forms.Plugin.Android.CustomReturnEntryRenderer.Init();
 
             LoadApplication(new App());
-        }
-
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
-        {
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
