e792fca5327fef2a4c82c0d11da521d83dc6e783
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-02-05 15:16:23 -0800

    Fixed Deserialization Error

--- AzureBlobStorageSampleApp.Functions/Functions/PostBlob.cs
+++ AzureBlobStorageSampleApp.Functions/Functions/PostBlob.cs
@@ -18,20 +18,22 @@ namespace AzureBlobStorageSampleApp.Functions
     {
         #region Methods
         [FunctionName(nameof(PostBlob))]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "PostBlob/{title}")][FromBody] PhotoBlobModel imageBlob, string title, ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "PostBlob/{title}")]HttpRequestMessage req, string title, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             try
             {
+                var imageBlob = await JsonService.DeserializeMessage<PhotoBlobModel>(req).ConfigureAwait(false);
                 var photo = await PhotosBlobStorageService.SavePhoto(imageBlob.Image, title).ConfigureAwait(false);
 
                 await PhotoDatabaseService.InsertPhoto(photo).ConfigureAwait(false);
 
                 return new CreatedResult(photo.Url, photo);
             }
-            catch
+            catch(Exception e)
             {
+                log.LogError(e, e.Message);
                 return new InternalServerErrorResult();
             }
         }
